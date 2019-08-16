a7001b0018ff18767789740d393fe8bdb77971b0
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-08-17 01:23:07 +0530

    Successfully Solved

---
+++ Dodecagon.cpp
@@ -0,0 +1,15 @@
+// Author: MOHAMMAD FAISAL
+/*  
+  Atcoder beginner contest #134
+  A - Dodecagon
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long r;
+  cin >> r;
+  long long ans = 3 * r * r;
+  cout<<ans<<endl;
+  return 0;
+}
