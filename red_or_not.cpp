a4e87799d066ae8fb9c2e053a2cdccd283091483
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-08-18 23:33:03 +0530

    solved successfully

---
+++ red_or_not.cpp
@@ -0,0 +1,16 @@
+// Author: Mohammad Faisal
+/*
+    AtCoder Beginner Contest 138
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  (n<3200) ? cout<<"red": cout<<s;
+  cout<<endl;
+  return 0;
+}
