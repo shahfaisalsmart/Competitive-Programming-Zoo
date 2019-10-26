0d6722c936ec79a4ea47f28f5153929e3a52a68a
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-10-26 16:52:58 +0530

    new file added

---
+++ Code Wizard/CircularListOfStudents.cpp
@@ -0,0 +1,23 @@
+// Author: Mohammad Faisal
+// BEGINNING WITH THE NAME OF ALMIGHTY GOD ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int i;
+        cin >> i;
+        int p;
+        cin >> p;
+        
+        cout<<(i+p)%12<<endl;
+        
+    }    
+    return 0;
+}
