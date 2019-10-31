977c77b5c45585ca11ccdcf0566deb46e276d641
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-10-31 10:47:33 +0530

    new file added

---
+++ atCoder Problems/RedOrNot.cpp
@@ -0,0 +1,21 @@
+// Author: Mohammad Faisal
+// BEGINNING WITH THE NAME OF ALMIGHTY GOD ALLAH
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  double ans =0;
+  for(int i=0;i<n;i++)
+  {
+    double x;
+    cin >> x;
+    ans +=(1/x);
+  }
+  
+  ans = (1/ans);
+  cout<<ans<<endl;
+  return 0;
+}
