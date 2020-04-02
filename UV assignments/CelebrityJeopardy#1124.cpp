951fa8110d7fdfd675ca003874addf0d1e5a9d2f
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2020-04-02 19:11:14 +0530

    file added

---
+++ UV assignments/CelebrityJeopardy#1124.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#define ll long long int
+#define faisal ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+using namespace std;
+
+int main()
+{
+    char s[100001];
+    while(gets(s))
+    {
+        printf("%s\n",s);
+    }
+    return 0;
+}
+
